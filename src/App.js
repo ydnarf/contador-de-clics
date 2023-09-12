6d7577006f9bbf58952a2c9f05c431c5c1f1d3cf
Frandy R. Nuñez <ydnarf13@hotmail.com>
2023-09-12 08:59:00 -0400

    Add: componentes(Boton, Contador) y styles(Boton, Contador)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
-import logoReact from './img/logo192.png';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import logoReact from './logo.svg';
+
 
 function App() {
+
+  const manejarClic = () => {
+    console.log('Clic');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
+
   return (
     <div className="App">
       <div className='react-logo-contenedor'>
-        <img 
-        className='react-logo'
-        src={logoReact}
-        logo='Logo de react'
-        />
+        <img className='react-logo' src={logoReact} logo='Logo de react' />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics='5' />
+        <Boton
+        texto='Clic'
+        esBotonClic={true}
+        manejarClic={manejarClic} />
+        <Boton
+        texto='Reiniciar'
+        esBotonClic={false}
+        manejarClic={reiniciarContador} />
       </div>
     </div>
   );
---
+++ src/componentes/Contador.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/Contador.css';
+
+
+function Contador( {numClics} ) {
+  return (
+    <div className='contador'>
+      {numClics}
+    </div>
+  );
+}
+
+export default Contador;
