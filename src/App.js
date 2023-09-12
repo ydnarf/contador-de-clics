fe259cc6a23199218661fdefb156ed684f6d5ffd
Frandy R. Nuñez <ydnarf13@hotmail.com>
2023-09-12 09:14:41 -0400

    delete: logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
-import logoReact from './logo.svg';
+
 
 
 function App() {
@@ -16,9 +16,6 @@ function App() {
 
   return (
     <div className="App">
-      <div className='react-logo-contenedor'>
-        <img className='react-logo' src={logoReact} logo='Logo de react' />
-      </div>
       <div className='contenedor-principal'>
         <Contador numClics='5' />
         <Boton
